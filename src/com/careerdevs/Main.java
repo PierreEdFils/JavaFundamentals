ec403133da7f3715846300ce5057217cfc55ca36
PierreEdFils <93496035+PierreEdFils@users.noreply.github.com>
2022-12-08 23:14:12 -0500

    Intermadiate level : TextBox Creating Objects

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,8 +1,18 @@
 package com.careerdevs;
 
+import com.careerdevs.intermediate.TextBox;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+//        System.out.println("Hello World");
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
     }
 }
--- src/com/careerdevs/intermediate/TextBox.java
+++ src/com/careerdevs/intermediate/TextBox.java
@@ -1,7 +1,7 @@
 package com.careerdevs.intermediate;
 
 public class TextBox {
-    public String text;// Field
+    public String text = "";// Field
 
     public void setText(String text){
         this.text =text;
