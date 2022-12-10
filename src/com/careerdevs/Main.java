1afd5407d32e7f3c9e45b00ee51a2cb96ec2cd0e
PierreEdFils <93496035+PierreEdFils@users.noreply.github.com>
2022-12-09 23:10:55 -0500

    Intermadiate level : Getters and Setters

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,18 +1,35 @@
 package com.careerdevs;
 
-import com.careerdevs.intermediate.TextBox;
+import com.careerdevs.intermediate.Browser;
+import com.careerdevs.intermediate.Employee;
 
 public class Main {
 
     public static void main(String[] args) {
 
 //        System.out.println("Hello World");
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
+//        var textBox1 = new TextBox();
+//        textBox1.setText("Box 1");
+//        System.out.println(textBox1.text.toUpperCase());
+//
+//        var textBox2 = new TextBox();
+//        textBox2.setText("Box 2");
+//        System.out.println(textBox2.text);
+//        var textBox2 = textBox1;
+//        textBox2.setText("Hello World");
+//        System.out.println(textBox1.text);
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+
+
+        var employee = new Employee();
+//        employee.setBaseSalary (50_000);
+//        employee.setHourlyRate(20);
+        int  wage = employee.calculateWage(10);
+        System.out.println(wage);
+
+//       var browser = new Browser();
+
+
     }
+
 }
---
+++ src/com/careerdevs/intermediate/Employee.java
@@ -0,0 +1,29 @@
+package com.careerdevs.intermediate;
+
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public int calculateWage( int extraHours) {
+        return  baseSalary + (extraHours * getHourlyRate());
+    }
+
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary <= 0)
+            throw  new IllegalArgumentException("Salary cannot be 0 or less ");
+        this.baseSalary = baseSalary;
+    }
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw  new IllegalArgumentException("hourly rate  cannot be 0 or negative ");
+        this.hourlyRate = hourlyRate;
+    }
+}
